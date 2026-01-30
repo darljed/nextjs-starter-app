d37c66e11178ecb062b739ec65df1eb020addf10
darljed <djam.devteam@gmail.com>
2026-01-30 23:08:49 +0800

    feat: added a welcome page and dashboard with shadcn sidebar as part of layout

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -1,52 +1,56 @@
 'use client'
+import AppSidebar from '@/components/AppSidebar'
 import SendVerificationEmail from '@/components/SendVerificationEmail'
 import SignOut from '@/components/SignOut'
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
 import { Badge } from '@/components/ui/badge'
-import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+import { SidebarProvider, SidebarTrigger, useSidebar } from '@/components/ui/sidebar'
 import { useAuth } from '@/providers/AuthProvider'
-import { LogOut, MailWarning } from 'lucide-react'
+import { MailWarning } from 'lucide-react'
+import React, { useEffect } from 'react'
 
 export default function Dashboard() {
+    const { user } = useAuth();
+    const { setOpenMobile } = useSidebar();
 
-    const { user } = useAuth()
+    useEffect(() => {
+        setOpenMobile(false)
+    },[])
 
-    
-    return (
-        <div>
-                <Card className="max-w-lg m-auto mt-20 ">
-                    <CardHeader>
-                        <CardTitle>
-                            Dashboard
-                        </CardTitle>
-                        <CardDescription>
-                            This page is protected and can only be viewed if you are properly authenticated.
-                        </CardDescription>
-                    </CardHeader>
-                    <CardContent>
-                        <p><strong>Name:</strong> {user?.name}</p>
-                        <p><strong>Email:</strong> {user?.email} {user?.emailVerified && <Badge variant="secondary">verified</Badge>}</p>
-                        {
-                            !user?.emailVerified && (
-                                <div className="mt-2">
-                                    <Alert>
-                                        <MailWarning />
-                                        <AlertTitle>Email not verified</AlertTitle>
-                                        <AlertDescription>
-                                            Please check your mailbox to verify your email.
-                                            <div>
-                                                <SendVerificationEmail />
-                                            </div>
-                                        </AlertDescription>
-                                    </Alert>
-                                </div>)
-                        }
-                    </CardContent>
-                    <CardFooter>
-                        <SignOut />
-                    </CardFooter>
-                </Card>
+  return (
+    <div>
+        <div className="w-full mt-20">
+            <Card className="max-w-lg m-auto mt-20 ">
+                <CardHeader>
+                    <CardTitle>
+                        Dashboard
+                    </CardTitle>
+                    <CardDescription>
+                        This page is protected and can only be viewed if you are properly authenticated.
+                    </CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <p><strong>Name:</strong> {user?.name}</p>
+                    <p><strong>Email:</strong> {user?.email} {user?.emailVerified && <Badge variant="secondary">verified</Badge>}</p>
+                    {
+                        !user?.emailVerified && (
+                            <div className="mt-2">
+                                <Alert>
+                                    <MailWarning />
+                                    <AlertTitle>Email not verified</AlertTitle>
+                                    <AlertDescription>
+                                        Please check your mailbox to verify your email.
+                                        <div>
+                                            <SendVerificationEmail />
+                                        </div>
+                                    </AlertDescription>
+                                </Alert>
+                            </div>)
+                    }
+                </CardContent>
+            </Card>
         </div>
-    )
+    </div>
+  )
 }
